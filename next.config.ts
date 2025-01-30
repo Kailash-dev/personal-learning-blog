3f3701a67659001b3addf0306cd694a68d1af2d3
kailash-dev <kailash514910@gmail.com>
2025-01-30 21:12:06 +0530

    next.config.ts changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+// next.config.js
+module.exports = {
+  // Enable static HTML export
+  exportTrailingSlash: true,
+  // Set the base path if deploying to a subdirectory
+  basePath: '/<repository-name>',
+}
